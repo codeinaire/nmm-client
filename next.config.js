aa8c9371352908ee110d3896c4ceebb1f586915f
John Codeinaire <johncodeinaire@gmail.com>
2020-02-18 17:30:17 +1100

    Update - envs and serverless

--- next.config.js
+++ next.config.js
@@ -2,23 +2,36 @@ const withCSS = require('@zeit/next-css')
 
 module.exports = withCSS({
   env: {
-    // HTTPS: true,
+    // !PROD ENVS - TODO - UPDATE EMPTY ENS
+    // * FB stuff + Auth0 + CORS stuff + HTTPS server for FB testing
+    // SERVER_URL:
+    //   'https://welqbatkyb.execute-api.ap-southeast-2.amazonaws.com/prod/nmm-app',
+    // FB_GROUP_ID: '548965739271455',
+    // FB_APP_ID: '695528800971318',
+    // APP_DOMAIN: 'nomeatmay.au.auth0.com',
+    // APP_CLIENT_ID: '4i4QDNDVF5rKsxsaRdMf6EmQvuQx0Gx4',
+    // AUDIENCE: 'https://nomeatmay.au.auth0.com/api/v2/',
+    // CLIENT_URL: 'https://d2hir6fuy7vez1.cloudfront.net',
+    // REDIRECT_URL: 'https://d2hir6fuy7vez1.cloudfront.net',
+    // LOGGER_URI: 'https://q5lmn150zh.execute-api.ap-southeast-2.amazonaws.com',
+    // ! LOCAL & PROD ENVS
+    // Don't worry about Ryan creating another account
+    CLOUDINARY_API: 'https://api.cloudinary.com/v1_1/codeinaire/image/upload',
+    IS_DEVELOPMENT: false,
+    // !LOCAL ENVS development
+    // * FB stuff + Auth0 + CORS stuff + HTTPS server for FB testing
+    REDIRECT_URL: 'http://localhost:3000/',
+    CLIENT_URL: 'http://localhost:3000',
+    SERVER_URL: 'http://localhost:4000/nmm-app',
     FB_GROUP_ID: '548965739271455',
     FB_APP_ID: '695528800971318',
     APP_DOMAIN: 'dev-s70wdmyk.au.auth0.com',
     APP_CLIENT_ID: 'Mn51EwPsHWiZUVRZJyNkNRZGKMikJoSE',
-    // CLIENT_URL: 'https://myapp.example:3000/',
-    CLIENT_URL: 'http://localhost:3000/',
-    // SERVER_URL: 'https://localhost:4000/nmm-app',
-    SERVER_URL: 'http://localhost:4000/nmm-app',
-    // SERVER_URL:
-    //   'https://0b7qzqr1t0.execute-api.ap-southeast-2.amazonaws.com/prod/nmm-app',
-    // AUDIENCE: 'https://dev-s70wdmyk.au.auth0.com/api/v2/',
-    // REDIRECT_URL: 'https://myapp.example:3000/',
-    REDIRECT_URL: 'http://localhost:3000/',
-    LOGGER_URI: 'https://zjy4b2tny0.execute-api.ap-southeast-2.amazonaws.com',
-    IS_DEVELOPMENT: true,
-    CLOUDINARY_API: 'https://api.cloudinary.com/v1_1/codeinaire/image/upload'
+    AUDIENCE: 'https://dev-s70wdmyk.au.auth0.com/api/v2/',
+    HTTPS: true,
+    CLIENT_URL: 'https://myapp.example:3000/',
+    SERVER_URL: 'https://localhost:4000/nmm-app',
+    REDIRECT_URL: 'https://myapp.example:3000/'
   },
   webpack: config => {
     // https://github.com/justadudewhohacks/face-api.js/issues/154
